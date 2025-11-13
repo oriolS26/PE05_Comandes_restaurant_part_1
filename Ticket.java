dd953b843ef4e364f933df8fef49d21a60df1aac
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2025-11-13 16:11:56 +0100

    Disseny del Menu principal

---
+++ Ticket.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Ticket {
+    //Variables globals
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Ticket program = new Ticket();
+        program.inici();
+    }
+
+    public void inici() {
+        System.out.println("Benvingut al sistema de venda d'entrades!");
+        System.out.println("-------------------------------------------");
+        System.out.println("===============MENU PRINCIPAL===============");
+        System.out.println("1: Crear nova comanda");
+        
+       switch (key) {
+        case value:
+            
+            break;
+       
+        default:
+            break;
+       }
+    }
+}
